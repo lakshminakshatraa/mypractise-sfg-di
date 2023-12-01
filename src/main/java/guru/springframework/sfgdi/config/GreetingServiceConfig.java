fef3866cee5817f360181d0ecc87bd1aec81c5d0
lakshminakshatraa@gmail.com <Bigbang@2023>
2023-11-30 22:23:53 -0500

    Refactored for Java Configuration-@Primary @Profile

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,10 +1,10 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -28,9 +28,26 @@ public class GreetingServiceConfig {
         return new PropertyInjectedGreetingService();
     }
 
-
     @Bean
     SetterInjectedGreetingService setterInjectedGreetingService(){
         return new SetterInjectedGreetingService();
+    }
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    @Profile({"EN"})
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Bean("i18nService")
+    @Profile({"ES","default"})
+    I18NSpanishService i18NSpanishService(){
+        return new I18NSpanishService();
     }
 }
